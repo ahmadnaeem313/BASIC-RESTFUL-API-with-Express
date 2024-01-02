8b21ede8efffe6e16329fccc944be4614d31ca6f
Ahmad Naeem <122064287+ahmadnaeem313@users.noreply.github.com>
2024-01-02 05:39:20 +0500

    updaet array

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const products = ["Laptop", "LCD", "Mobile"];
+const products=["AHMAD NAEEM ", " HAIDER ALI " ,"USMAN CHEMMA ", " HUSNAIN SHAH " ,"AHMAD KHOKHAR ", " ABU-BAKAR JUTT ","HASSAN HABIB", "BILAL LIAQUAT ","ABDULLAH SHEIKH"," ASIM USMAN","SAHIL WASEEM","DOCTOR FARRUKH RIZWAN"]
 
 app.get("/", function (req, res) {
   res.send("Hello World");
